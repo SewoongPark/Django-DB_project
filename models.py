570a0c6174d954da382b7cb2437e7cbc4a51141e
SewoongPark <98893325+SewoongPark@users.noreply.github.com>
2023-06-16 10:56:57 +0900

    Add files via upload

--- models.py
+++ models.py
@@ -17,40 +17,39 @@ class Champions(models.Model):
 class Champion_rate(models.Model):
   champ_id = models.CharField(primary_key=True, max_length=255)
   champion = models.ForeignKey(Champions, on_delete=models.CASCADE)
-  라인 = models.CharField(max_length=255, null=True),
-  승률 = models.CharField(max_length=255, null=True),
-  픽률 = models.CharField(max_length=255, null=True),
-  밴률 = models.CharField(max_length=255, null=True),
+  line = models.CharField(max_length=255, null=True)
+  winrate = models.CharField(max_length=255, null=True)
+  pickrate = models.CharField(max_length=255, null=True)
+  banrate = models.CharField(max_length=255, null=True)
 
 class Champion_story(models.Model):
   champ_id = models.CharField(primary_key=True, max_length=255)
   champion = models.ForeignKey(Champions, on_delete=models.CASCADE)
-  # champion = models.CharField(primary_key=True, max_length=255)
   url = models.CharField(max_length=255, null=True),
-  text = models.CharField(max_length=255, null=True),
-  story = models.CharField(max_length=255,null=True),
+  text = models.CharField(max_length=255, null=True)
+  story = models.CharField(max_length=255,null=True)
   
 class Champion_tip(models.Model):
   champ_id = models.CharField(primary_key=True, max_length=255)
   champion = models.ForeignKey(Champions, on_delete=models.CASCADE)
-  key = models.CharField(max_length=255, null=True),
-  id = models.IntegerField(max_length=255, null=True),
-  image_url = models.CharField(max_length=255,null=True),
-  enemy_tips = models.CharField(max_length=255, null=True),
-  ally_tips = models.CharField(max_length=255, null=True),
+  key = models.CharField(max_length=255, null=True)
+  id = models.IntegerField(null=True)
+  imageurl = models.CharField(max_length=255,null=True)
+  enemytips = models.CharField(max_length=255, null=True)
+  allytips = models.CharField(max_length=255, null=True)
   
 class Champion_counter(models.Model):
   champ_id = models.CharField(primary_key=True, max_length=255)
   champion = models.ForeignKey(Champions, on_delete=models.CASCADE)
-  counter_name = models.CharField(primary_key=True, max_length=255, null=True),
-  승률 = models. CharField(max_length=255, null=True),
+  countername = models.CharField(max_length=255, null=False)
+  winrate = models. CharField(max_length=255, null=True)
   
 
-class Champion_skill(models.Model):
+class ChampionSkill(models.Model):
   champ_id = models.CharField(primary_key=True, max_length=255)
   champion = models.ForeignKey(Champions, on_delete=models.CASCADE)
-  passive = models.CharField(max_length=255, null=True),
-  Q_name = models.IntegerField(max_length=255, null=True),
-  W_name = models.CharField(max_length=255, null=True),
-  E_name = models.CharField(max_length=255, null=True),
-  R_name = models.CharField(max_length=255, null=True),  
+  passive = models.CharField(max_length=255, null=True)
+  Q_name = models.CharField(max_length=255, null=True)
+  W_name = models.CharField(max_length=255, null=True)
+  E_name = models.CharField(max_length=255, null=True)
+  R_name = models.CharField(max_length=255, null=True)
