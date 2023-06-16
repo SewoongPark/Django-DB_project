f86d51e63eb43e79fc931f5e413e24d24bed80a0
SewoongPark <98893325+SewoongPark@users.noreply.github.com>
2023-06-16 09:45:54 +0900

    Add files via upload

--- models.py
+++ models.py
@@ -5,35 +5,52 @@ from django.core import validators
 # # Create your models here.
 class User(models.Model):
   id = models.AutoField(primary_key=True)
-  login_id = models.CharField(max_length=255)
-  password = models.CharField(max_length=255)
+  login_id = models.CharField(max_length=255, null=False)
+  password = models.CharField(max_length=255, null=False)
   
-class Champion(models.Model):
- 
+class Champions(models.Model):
   champion = models.CharField(primary_key=True, max_length=255)
-  line = models.CharField(max_length=512)
-  position = models.CharField(max_length=100)
-  CC = models.CharField(max_length=512) 
-  #이미지 url로 가져올 경우를 대비해서 varchar로 설정
-  # user = models.ForeignKey("User", on_delete=models.CASCADE)
+  line = models.CharField(max_length=512, null=True)
+  position = models.CharField(max_length=100, null=True)
+  CC = models.CharField(max_length=512, null=True) 
   
-# class Champion_rate(models.Model):
-#   champion = models.ForeignKey(Champion, on_delete=models.CASCADE)
-#   라인 = models.CharField(max_length=255),
-#   승률 = models.CharField(max_length=255),
-#   픽률 = models.CharField(max_length=255),
-#   밴률 = models.CharField(max_length=255),
+class Champion_rate(models.Model):
+  champ_id = models.CharField(primary_key=True, max_length=255)
+  champion = models.ForeignKey(Champions, on_delete=models.CASCADE)
+  라인 = models.CharField(max_length=255, null=True),
+  승률 = models.CharField(max_length=255, null=True),
+  픽률 = models.CharField(max_length=255, null=True),
+  밴률 = models.CharField(max_length=255, null=True),
+
+class Champion_story(models.Model):
+  champ_id = models.CharField(primary_key=True, max_length=255)
+  champion = models.ForeignKey(Champions, on_delete=models.CASCADE)
+  # champion = models.CharField(primary_key=True, max_length=255)
+  url = models.CharField(max_length=255, null=True),
+  text = models.CharField(max_length=255, null=True),
+  story = models.CharField(max_length=255,null=True),
   
-# class Comment(models.Model):
-#   comment_id = models.CharField(primary_key=True, max_length=100)
-#   user_id = models.AutoField(models.ForeignKey("User", on_delete=models.CASCADE))  
-#   champ = models.ForeignKey(Champion, on_delete=models.CASCADE)
-#   user_comment = models.CharField(max_length=512)
-#   total_comment = models.CharField(max_length=512)
-#   best_comment = models.CharField(max_length=512)
-#   rating = models.IntegerField(max_length=3)
+class Champion_tip(models.Model):
+  champ_id = models.CharField(primary_key=True, max_length=255)
+  champion = models.ForeignKey(Champions, on_delete=models.CASCADE)
+  key = models.CharField(max_length=255, null=True),
+  id = models.IntegerField(max_length=255, null=True),
+  image_url = models.CharField(max_length=255,null=True),
+  enemy_tips = models.CharField(max_length=255, null=True),
+  ally_tips = models.CharField(max_length=255, null=True),
   
-# class WordCloud(models.Model):
-#   cloud_id = models.AutoField(primary_key=True, max_length=100)
-#   champ_id = models.ForeignKey("Champ", on_delete=models.CASCADE)
+class Champion_counter(models.Model):
+  champ_id = models.CharField(primary_key=True, max_length=255)
+  champion = models.ForeignKey(Champions, on_delete=models.CASCADE)
+  counter_name = models.CharField(primary_key=True, max_length=255, null=True),
+  승률 = models. CharField(max_length=255, null=True),
   
+
+class Champion_skill(models.Model):
+  champ_id = models.CharField(primary_key=True, max_length=255)
+  champion = models.ForeignKey(Champions, on_delete=models.CASCADE)
+  passive = models.CharField(max_length=255, null=True),
+  Q_name = models.IntegerField(max_length=255, null=True),
+  W_name = models.CharField(max_length=255, null=True),
+  E_name = models.CharField(max_length=255, null=True),
+  R_name = models.CharField(max_length=255, null=True),  
